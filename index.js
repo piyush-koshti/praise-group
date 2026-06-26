e7df7cd9e115d0c5acdf5e51e9263252fa47f630
piyush-saasinte <piyush@saasintegrator.com>
2026-06-26 19:32:02 +0530

    .

--- index.js
+++ index.js
@@ -55,6 +55,21 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+// create one more products api and pass staic data
+app.get('/api/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 200 },
+        { id: 3, name: 'Product 3', price: 300 }
+    ];
+
+    res.json({
+        success: true,
+        total: products.length,
+        data: products
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
