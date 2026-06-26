4f5d1e5e5aa28c41bf143c42b4f757220312805e
piyush-saasinte <piyush@saasintegrator.com>
2026-06-26 20:09:39 +0530

    add products

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ app.get('/api/products', (req, res) => {
     const products = [
         { id: 1, name: 'Product 1', price: 100 },
         { id: 2, name: 'Product 2', price: 200 },
-        { id: 3, name: 'Product 3', price: 300 }
+        { id: 3, name: 'Product 3', price: 300 },
+        { id: 3, name: 'Product 4', price: 400 }
     ];
 
     res.json({
