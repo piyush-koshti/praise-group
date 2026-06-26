a190ab4b71eb6a51da61e224522cda06d84e4305
piyush-saasinte <piyush@saasintegrator.com>
2026-06-26 20:35:53 +0530

    remove 10 record in product

--- index.js
+++ index.js
@@ -67,17 +67,7 @@ app.get('/api/products', (req, res) => {
         { id: 7, name: 'Product 7', price: 700 },
         { id: 8, name: 'Product 8', price: 800 },
         { id: 9, name: 'Product 9', price: 900 },
-        { id: 10, name: 'Product 10', price: 1000 },
-        { id: 11, name: 'Product 11', price: 1100 },
-        { id: 12, name: 'Product 12', price: 1200 },
-        { id: 13, name: 'Product 13', price: 1300 },
-        { id: 14, name: 'Product 14', price: 1400 },
-        { id: 15, name: 'Product 15', price: 1500 },
-        { id: 16, name: 'Product 16', price: 1600 },
-        { id: 17, name: 'Product 17', price: 1700 },
-        { id: 18, name: 'Product 18', price: 1800 },
-        { id: 19, name: 'Product 19', price: 1900 },
-        { id: 20, name: 'Product 20', price: 2000 }
+        { id: 10, name: 'Product 10', price: 1000 }
     ];
 
     res.json({
